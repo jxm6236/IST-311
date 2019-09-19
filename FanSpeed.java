01b588c9f1ba02b0a393eb903c822dd0ee4f93a3
jxm6236 <54914673+jxm6236@users.noreply.github.com>
2019-09-18 22:49:28 -0400

    Fan Speed 2

--- FanSpeed.java
+++ FanSpeed.java
@@ -14,6 +14,15 @@ public class FanSpeed {
     /**
      * @param args the command line arguments
      */
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+
+    private int speed = SLOW;
+    private boolean on = false;
+    private double radius = 5;
+    private String color = "blue";
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
