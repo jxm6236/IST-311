fe4b39057cd4bf9d966028ee1da454ac64289e28
jxm6236 <54914673+jxm6236@users.noreply.github.com>
2019-09-29 21:22:01 -0400

    Added contructor and Getters/Setters

--- MyDate.java
+++ MyDate.java
@@ -11,15 +11,41 @@ package mydate;
  */
 public class MyDate {
     
-    final int year;
-    final int month;
-    final int day;
+    private int year;
+    private int month;
+    private int day;
     
-    public MyDate(int y, int m, int d) {
-        year = y;
-        month = m;
-        day = d;  
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;  
     }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+    
+    
 
     /**
      * @param args the command line arguments
