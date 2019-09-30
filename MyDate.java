5c4e2fc811bfc8a80b88cbf460e644deda79055d
jxm6236 <54914673+jxm6236@users.noreply.github.com>
2019-09-29 20:41:04 -0400

    Add files via upload

---
+++ MyDate.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydate;
+
+/**
+ *
+ * @author jmhaz
+ */
+public class MyDate {
+    
+    final int year;
+    final int month;
+    final int day;
+    
+    public MyDate(int y, int m, int d) {
+        year = y;
+        month = m;
+        day = d;  
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
