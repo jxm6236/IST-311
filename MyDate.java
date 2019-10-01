08c33ff5199a781a71ec79d1c54b78c9c5823680
jxm6236 <54914673+jxm6236@users.noreply.github.com>
2019-09-30 22:49:24 -0400

    Fixing

--- MyDate.java
+++ MyDate.java
@@ -1,10 +1,10 @@
-/*
-/***************************************
-* Filename: MyDate.java
-* Short description: This class displays two dates
-* @author James McGettigan
-* @version  9/30/2019
-***************************************/
+/**
+	File name: MyDate.java
+	Short description: Displays 2 Dates
+	IST 242 Assignment:
+	@author Jimmy McGettigan
+	@version 1.01  9/30/2019
+*/
 package mydate;
 
 /**
@@ -12,24 +12,53 @@ package mydate;
  * @author jmhaz
  */
 public class MyDate {
+    //Initialize variables
+    final int jan = 0;
     
     private int year;
     private int month;
     private int day;
     
-    public MyDate(int year, int month, int day) {
-        this.year = year;
-        this.month = month;
-        this.day = day;  
+    public MyDate() {
+        //No-Arg Constructor
+        
+        //default values
+        year = 1970;
+        month = 1;
+        day = 1;  
     }
     
-    public MyDate() {
-        year = 1970;
-        month = 0;
-        day = 1;
-        
-
+    public MyDate(long elapsedTime) {
+        //Pass in data
+        this.year = year;
+        this.month = month;
+        this.day = day;
     }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyDate d1 = new MyDate();
+        MyDate d2 = new MyDate(34355555133101L);
+        
+        System.out.println("Date 1: " + d1.getYear() + "/" +  d1.getMonth() + 
+                "/" + d1.getDay());
+        System.out.println("Date 2: " + d2.getYear() + d2.getMonth() + d2.getDay());
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+    
 
     public int getYear() {
         return year;
@@ -39,31 +68,10 @@ public class MyDate {
         return month;
     }
 
-
     public int getDay() {
         return day;
     }
-
     
-    public MyDate(long elapsedTime) {
-        
-    }
     
-    public void setDate(long elapsedTime) {
-        
-    }
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        MyDate d1 = new MyDate();
-        MyDate d2 = new MyDate(34355555133101L);
-        
-        System.out.println("Date 1: yyyy/mm/dd" + d1.getYear() + d1.getMonth() + 
-                d1.getDay());
-        System.out.println("Date 2: yyyy/mm/dd" + "1970" + "January" + 
-                "1");
-    }
     
 }
